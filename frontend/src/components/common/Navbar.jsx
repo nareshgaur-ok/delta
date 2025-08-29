a1c2addcc7e3251551926921db02ca0bc5607868
Nmart <shop.sharmamart@gmail.com>
2025-08-30 00:05:31 +0530

    Initial commit - Nmart project

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <nav className="container mx-auto flex items-center justify-between py-4 px-6 ">
         <div>
           <Link to="/" className="text-2xl font-medium">
-            Vrindavan
+            N{" "} Mart
           </Link>
         </div>
         {/* // center navigation */}
